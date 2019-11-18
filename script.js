b7cf87ab1e5afa8bc0ba4a3dfaae04698d99adf6
AlmaDev <44857611+AlmaDev@users.noreply.github.com>
2019-11-18 13:53:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ window.onload = function() {
   var ctx = canvas.getContext("2d");
   var image = new Image();
   var map = new Image();
+  image.onload = function() {
+    ctx.drawImage(image, 0, 0, image.width/6, image.height/6);
+  }
   map.onload = function() {
     ctx.drawImage(map, 0, 0);
-    ctx.drawImage(image, 0, 0, image.width/6, image.height/6);
   }
   image.src = "https://treemastersllc.com/files/2019/03/_4.png";
   map.src = "http://pluspng.com/img-png/png-usa-outline-us-map-outline-png-87-detailed-with-us-map-outline-png-2400.png";
