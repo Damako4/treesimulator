a316c9463cb8f74afa515161a451b2bbf832d696
AlmaDev <44857611+AlmaDev@users.noreply.github.com>
2019-11-18 13:16:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 window.onload = function() {
   var canvas = document.getElementById("spawner");
   var ctx = canvas.getContext("2d");
-  var myImg = new Image();
-  img.onload = function() {
-    context.drawImage(myImg, 0, 0);
+  var myCanvas = document.getElementById('my_canvas_id');
+  var ctx = myCanvas.getContext('2d');
+  var img = new Image;
+  img.onload = function(){
+    ctx.drawImage(img,0,0); // Or at whatever offset you like
   };
-  img.src = 'tree_icon.png';
+  img.src = "http://apextreeandearth.com/files/2014/04/tree1.png";
 };
