dad7847c4654a143e09a45053834c018252114c5
AlmaDev <44857611+AlmaDev@users.noreply.github.com>
2019-11-18 13:17:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ window.onload = function() {
   var canvas = document.getElementById("spawner");
   var ctx = canvas.getContext("2d");
   var myCanvas = document.getElementById('my_canvas_id');
-  var ctx = myCanvas.getContext('2d');
-  var img = new Image;
-  img.onload = function(){
-    ctx.drawImage(img,0,0); // Or at whatever offset you like
+  var myImg = new Image();
+  img.onload = function() {
+     context.drawImage(myImg, 0, 0);
   };
-  img.src = "http://apextreeandearth.com/files/2014/04/tree1.png";
+  img.src = 'https://www.tutorialspoint.com/images/seaborn-4.jpg?v=2';
 };
