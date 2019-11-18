3c91e68f42004d6279ce606e24cd8c8df4430c05
AlmaDev <44857611+AlmaDev@users.noreply.github.com>
2019-11-18 13:07:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function() {
   var canvas = document.getElementById("spawner");
   var ctx = canvas.getContext("2d");
-  var img = document.getElementById("tree");
+  var img = document.getElementById("tree_icon.png");
   ctx.drawImage(img, 10, 10);
 };
