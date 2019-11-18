6c5dd7c4cb9542ef9418611bd3034b701dcc25fb
AlmaDev <44857611+AlmaDev@users.noreply.github.com>
2019-11-18 13:40:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.onload = function() {
   var ctx = canvas.getContext("2d");
   var image = new Image();
   image.onload = function() {
-     ctx.drawImage(image, 0, 0, image.width/3, image.height/3);
+     ctx.drawImage(image, 0, 0, image.width/6, image.height/6);
   };
   image.src = "https://treemastersllc.com/files/2019/03/_4.png";
 };
