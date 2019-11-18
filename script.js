6db142a12dc0a232e74a30c6748d518e66ff024f
AlmaDev <44857611+AlmaDev@users.noreply.github.com>
2019-11-18 13:34:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ window.onload = function() {
   var canvas = document.getElementById("spawner");
   var ctx = canvas.getContext("2d");
   var image = new Image();
-  image.width = image.width/3;
-  image.height = image.height/3;
   image.onload = function() {
-     ctx.drawImage(image, 0, 0);
+     ctx.drawImage(image, 0, 0, image.width/3, image.height/3);
   };
   image.src = "http://apextreeandearth.com/files/2014/04/tree1.png";
 };
