e45615bd42c181989364e0d8b116f64ac985466d
AlmaDev <44857611+AlmaDev@users.noreply.github.com>
2019-11-19 08:24:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 window.onload = function() {
   var canvas = document.getElementById("spawner");
   var ctx = canvas.getContext("2d");
+  ctx.canvas.width  = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
   const imageURL = ["https://treemastersllc.com/files/2019/03/_4.png","http://pluspng.com/img-png/png-usa-outline-us-map-outline-png-87-detailed-with-us-map-outline-png-2400.png"]; // list of image URLs
   const images = [];
   var imageCount = 0;
