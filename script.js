c370f51effd7a009af1475168c0a1c8ebcce7fa9
AlmaDev <44857611+AlmaDev@users.noreply.github.com>
2019-11-19 12:58:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -38,6 +38,6 @@ window.onload = function() {
     var mousePos = getMousePos(canvas, evt);
     drawTree(mousePos.x-15, mousePos.y-15);
     var pixel = ctx.getImageData(mousePos.x, mousePos.y);
-    alert(pixel.data[3]);
+    window.alert(pixel.data[3]);
   }, false);
 };
