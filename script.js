294aa035686b3fc9d3712a72fc0f2029eb26f16e
AlmaDev <44857611+AlmaDev@users.noreply.github.com>
2019-11-19 08:20:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 window.onload = function() {
   var canvas = document.getElementById("spawner");
   var ctx = canvas.getContext("2d");
-  var image = new Image();
-  var map = new Image();
-  image.onload = function() {
-    ctx.drawImage(image, 0, 0, image.width/6, image.height/6);
+  const imageURL = ["https://treemastersllc.com/files/2019/03/_4.png","http://pluspng.com/img-png/png-usa-outline-us-map-outline-png-87-detailed-with-us-map-outline-png-2400.png"]; // list of image URLs
+  const images = [];
+  var imageCount = 0;
+  function allLoaded(){
+      ctx.drawImage(images[1],0,0);
+      ctx.drawImage(images[0],0,0);
   }
-  map.onload = function() {
-    ctx.drawImage(map, 0, 0);
-  }
-  image.src = "https://treemastersllc.com/files/2019/03/_4.png";
-  map.src = "http://pluspng.com/img-png/png-usa-outline-us-map-outline-png-87-detailed-with-us-map-outline-png-2400.png";
+  imageURL.forEach(src => {
+       const image = new Image();
+       image.src = src;
+       image.onload = ()=>{ 
+           imageCount += 1;
+           if(imageCount === imageURL.length){
+               allLoaded();
+           }
+       }
+       images.push(image);
+
+  });
 };
