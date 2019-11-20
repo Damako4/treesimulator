d3358bb8441453ba8ace73c3ec668ac03ee72262
AlmaDev <44857611+AlmaDev@users.noreply.github.com>
2019-11-19 21:20:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,11 @@ window.onload = function() {
   var ctx = canvas.getContext("2d");
   ctx.canvas.width  = window.innerWidth;
   ctx.canvas.height = window.innerHeight;
-  const imageURL = ["https://treemastersllc.com/files/2019/03/_4.png","http://pluspng.com/img-png/png-usa-outline-us-map-outline-png-87-detailed-with-us-map-outline-png-2400.png"]; // list of image URLs
+  const imageURL = ["tree.png","map.png"];
   const images = [];
   var imageCount = 0;
-  function getMousePos(canvas, evt) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-      x: evt.clientX - rect.left,
-      y: evt.clientY - rect.top
-    };
-  }
   function allLoaded(){
-      ctx.drawImage(images[1],0,0,images[1].width/2.5,images[1].height/2.5);
+      ctx.drawImage(images[1],0,0,images[1].width,images[1].height);
   }
   function drawTree(x,y){
       ctx.drawImage(images[0],x,y,images[0].width/8,images[0].height/8);
@@ -28,16 +21,40 @@ window.onload = function() {
                allLoaded();
            }
        }
+       
        images.push(image);
 
   });
-  function draw(evt) {
-      var pos = getMousePos(canvas, evt);
-  }
-  canvas.addEventListener("mousedown", function(evt) {
-    var mousePos = getMousePos(canvas, evt);
-    drawTree(mousePos.x-15, mousePos.y-15);
-    var pixel = ctx.getImageData(mousePos.x, mousePos.y,1,1).data;
-    alert("clicked");
-  }, false);
+function findPos(obj) {
+    var curleft = 0, curtop = 0;
+    if (obj.offsetParent) {
+        do {
+            curleft += obj.offsetLeft;
+            curtop += obj.offsetTop;
+        } while (obj = obj.offsetParent);
+        return { x: curleft, y: curtop };
+    }
+    return undefined;
+}
+
+function rgbToHex(r, g, b) {
+    if (r > 255 || g > 255 || b > 255)
+        throw "Invalid color component";
+    return ((r << 16) | (g << 8) | b).toString(16);
+}
+
+
+$('#spawner').mousedown(function(e) {
+
+    var pos = findPos(this);
+    var x = e.pageX - pos.x;
+    var y = e.pageY - pos.y;
+    drawTree(x,y)
+    var coord = "x=" + x + ", y=" + y;
+    var c = this.getContext('2d');
+    var p = c.getImageData(x, y, 1, 1).data; 
+    var hex = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
+    if (hex)
+});
+
 };
